c5963b3cbac591736eb1ed686f6a8715fe330c42
enoshima7 <duanjipeng0328@gmail.com>
2022-09-21 15:15:30 +0800

    use git hook to convert

--- scripts/convert.ts
+++ scripts/convert.ts
@@ -1,18 +1,18 @@
-import { resolve } from 'path';
-import {convertor} from 'md-img-convertor'
+import { resolve } from 'path'
 import fs from 'fs'
+import { convertor } from 'md-img-convertor'
 import glob from 'glob'
-const dir = resolve(__dirname,'../notes')
-glob('**/*.md',{
-  ignore: ['**/node_modules/**','**/README.md']
-},(err,mdFiles)=>{
-  if(err){
-    console.warn(err);
-  }else{
+glob('**/*.md', {
+  ignore: ['**/node_modules/**', '**/README.md'],
+}, (err, mdFiles) => {
+  if (err) {
+    console.warn(err)
+  }
+  else {
     for (const file of mdFiles) {
-      const source = fs.readFileSync(resolve(__dirname,`../${file}`),{encoding:'utf-8'})
-      const res = convertor(source,{imgDist:resolve(__dirname,'../assets'),folderLink:'https://raw.githubusercontent.com/kitiho/leetcode/main/assets'})
-      fs.writeFileSync(resolve(__dirname,`../${file}`),res,{encoding:'utf-8'})
+      const source = fs.readFileSync(resolve(__dirname, `../${file}`), { encoding: 'utf-8' })
+      const res = convertor(source, { imgDist: resolve(__dirname, '../assets'), folderLink: 'https://raw.githubusercontent.com/kitiho/leetcode/main/assets' })
+      fs.writeFileSync(resolve(__dirname, `../${file}`), res, { encoding: 'utf-8' })
     }
   }
 })
